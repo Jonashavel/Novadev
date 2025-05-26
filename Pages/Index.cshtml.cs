5712b81524c39a4bca2b686678357fb56518ac3e
haveljo <haveljo@student.uzlabina.cz>
2025-05-26 16:28:12 +0200

    Oprava

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,33 +1,56 @@
-using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Newtonsoft.Json;
+//using Newtonsoft.Json;
 
 namespace Novadev.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-namespace Novadev.Pages;
 
-public class IndexModel : PageModel
-{
-    private readonly ILogger<IndexModel> _logger;
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
 
+        public IActionResult OnPostSearch(string query)
+        {
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                return BadRequest("Please enter a location to search.");
+            }
 
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
+            var result = new
+            {
+                query = query,
+                places = new[]
+                {
+                    new { id = "1", name = "Bank A", address = "Street 1", phone = "123-456", latitude = 34.039737, longitude = -118.270293 },
+                    new { id = "2", name = "ATM B", address = "Street 2", phone = "789-012", latitude = 34.040000, longitude = -118.271000 },
+                }
+            };
 
-    public void OnGet()
-    {
+            return new JsonResult(result);
+        }
 
-    }
-    public string? SearchInput { get; set; }
+        public IActionResult OnPostItemClicked(string id)
+        {
+            var details = new
+            {
+                id = id,
+                name = id == "1" ? "Bank A" : "ATM B",
+                address = id == "1" ? "Street 1" : "Street 2",
+                phone = id == "1" ? "123-456" : "789-012",
+                description = id == "1" ? "A well-known bank" : "A local ATM",
+                latitude = id == "1" ? 34.039737 : 34.040000,
+                longitude = id == "1" ? -118.270293 : -118.271000
+            };
 
-    public void OnPost()
-    {
+            return new JsonResult(details);
+        }
 
+        public void OnGet()
+        {
+        }
     }
 }
--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -1,56 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Newtonsoft.Json;
 
-namespace Novadev.Pages
+namespace Novadev.Pages;
+
+public class PrivacyModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
     {
-        private readonly ILogger<IndexModel> _logger;
+        _logger = logger;
+    }
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult OnPostSearch(string query)
-        {
-            if (string.IsNullOrWhiteSpace(query))
-            {
-                return BadRequest("Please enter a location to search.");
-            }
-
-            var result = new
-            {
-                query = query,
-                places = new[]
-                {
-                    new { id = "1", name = "Bank A", address = "Street 1", phone = "123-456", latitude = 34.039737, longitude = -118.270293 },
-                    new { id = "2", name = "ATM B", address = "Street 2", phone = "789-012", latitude = 34.040000, longitude = -118.271000 },
-                }
-            };
-
-            return new JsonResult(result);
-        }
-
-        public IActionResult OnPostItemClicked(string id)
-        {
-            var details = new
-            {
-                id = id,
-                name = id == "1" ? "Bank A" : "ATM B",
-                address = id == "1" ? "Street 1" : "Street 2",
-                phone = id == "1" ? "123-456" : "789-012",
-                description = id == "1" ? "A well-known bank" : "A local ATM",
-                latitude = id == "1" ? 34.039737 : 34.040000,
-                longitude = id == "1" ? -118.270293 : -118.271000
-            };
-
-            return new JsonResult(details);
-        }
-
-        public void OnGet()
-        {
-        }
+    public void OnGet()
+    {
     }
 }
